cfa37043cbec2e49a79c0c7f849ae9b36cb8ab9b
Danil Karpushevich <noname211070@gmai.com>
2022-09-29 20:21:23 +0600

    feat: 27.09

---
+++ 27.09/task 1/script.js
@@ -0,0 +1,6 @@
+let sameAwesomeVar = 1;
+let same_awesome_var = 2;
+let SameAwesomeVar = 3;
+alert(sameAwesomeVar);
+alert(same_awesome_var);
+alert(SameAwesomeVar);
---
+++ 27.09/task 2/script.js
@@ -0,0 +1,4 @@
+let name1 = "Danil";
+let name2 = "Karpushevich";
+name2 = name1;
+alert(name2);
