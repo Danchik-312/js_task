1178eaf96f32a6e10044470295479570445f5d26
Danil <noname211070@gmail.com>
2022-11-05 13:44:14 +0300

    fix: 03.11

--- 03.11/task 1/script.js
+++ 03.11/task 1/script.js
@@ -1,11 +1,13 @@
 const notification = document.querySelector('.notification');
 const btn1 = document.querySelector('#btn1');
 
-function showNoitification (text = 'hello', className = 'alert-danger'){
+function showNoitification ({text = 'hello', className = 'alert-primary'}){
    notification.classList.add(className);
    notification.innerText = text;
 
    setTimeout(() => notification.classList.remove(className), 1500);
 }
 
-btn1.addEventListener('click', showNoitification);
+btn1.addEventListener('click', showNoitification,);
+
+//alert-primary, alert-secondary, alert-success, alert-danger
