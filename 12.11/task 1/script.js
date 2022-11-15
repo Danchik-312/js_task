089480d9a3c895f33c0de92247bf840b9e9b9d02
Danil Karpushevich <noname211070@gmail.com>
2022-11-15 14:29:32 +0300

    feat: 12.11

---
+++ 12.11/task 1/script.js
@@ -0,0 +1,9 @@
+document.querySelector('#first-block').addEventListener('click', () => {
+    document.querySelector('#first-block').classList.toggle('red');
+})
+document.querySelector('#second-block').addEventListener('click', () => {
+    document.querySelector('#second-block').classList.toggle('blue');
+})
+document.querySelector('#third-block').addEventListener('click', () => {
+    document.querySelector('#third-block').classList.toggle('gray');
+})
